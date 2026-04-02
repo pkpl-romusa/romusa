080fb4a9ebd70820e7081b6c08ed5e2601239ad7
firoszfa <firoszufa20@gmail.com>
2026-04-02 23:45:35 +0700

    dummy for testing

--- biodata/views.py
+++ biodata/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from accounts.utils import check_is_group_member
 
 def index(request):
     return HttpResponse("<h1>Halaman Biodata Kelompok ROMUSA</h1><p>Status: Fondasi Django Berhasil!</p>")
+
+def biodata_view(request):
+    # Dapatkan status otorisasi (True / False)
+    is_group_member = check_is_group_member(request.user)
+            
+    context = {
+        'is_group_member': is_group_member,
+    }
+    return render(request, 'biodata.html', context)
