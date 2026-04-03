6e272c38efe1546410257a59bfe4e48e25601a9b
firoszfa <firoszufa20@gmail.com>
2026-04-03 14:16:34 +0700

    Created model for global theme setting

--- biodata/models.py
+++ biodata/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class GlobalSetting(models.Model):
+    tema = models.CharField(max_length=50, default='', blank=True)
+    font = models.CharField(max_length=50, default='', blank=True)
+
+    def __str__(self):
+        return "Pengaturan Tampilan Global"
