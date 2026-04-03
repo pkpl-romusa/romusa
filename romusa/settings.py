5d0b7e67d1c97f915b1693cb4062de2b9fb8c6bb
firoszfa <firoszufa20@gmail.com>
2026-04-03 14:37:09 +0700

    changed to supabase for database

--- romusa/settings.py
+++ romusa/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -95,10 +96,10 @@ WSGI_APPLICATION = 'romusa.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 
