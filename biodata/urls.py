f0079ca21113df9540528e3c6be9215ce01b6a8d
firoszfa <firoszufa20@gmail.com>
2026-04-03 14:11:50 +0700

    implemented ubah_tampilan for theme and font

--- biodata/urls.py
+++ biodata/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ubah-tampilan/', views.ubah_tampilan, name='ubah_tampilan'),
 ]
--- biodata/views.py
+++ biodata/views.py
@@ -1,13 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from accounts.utils import check_is_group_member
 
 def index(request):
     
+    tema_aktif = request.session.get('tema_css', 'tema-terang')
+    font_aktif = request.session.get('font_css', 'font-sans')
+    
     is_group_member = check_is_group_member(request.user)
             
     context = {
         'is_group_member': is_group_member,
+        'class_tema_aktif': tema_aktif,
+        'class_font_aktif': font_aktif,
     }
     
     return render(request, 'index.html', context)
+
+
+def ubah_tampilan(request):
+    if request.method == 'POST':
+        action = request.POST.get('action')
+
+        if action == 'ganti_tema':
+            tema_sekarang = request.session.get('tema_css', '')
+            if tema_sekarang == '':
+                request.session['tema_css'] = 'dark-theme'
+            else:
+                request.session['tema_css'] = ''
+
+        elif action == 'ganti_font':
+            font_sekarang = request.session.get('font_css', '')
+            if font_sekarang == '':
+                request.session['font_css'] = 'serif-theme'
+            else:
+                request.session['font_css'] = ''
+
+    return redirect('index')
