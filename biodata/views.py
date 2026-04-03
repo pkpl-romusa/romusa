998bb289d00634c1594f2a63e5cab4428d1ab51f
firoszfa <firoszufa20@gmail.com>
2026-04-03 14:19:16 +0700

    changed ubah_tampilan for global setting

--- biodata/views.py
+++ biodata/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from accounts.utils import check_is_group_member
+from .models import GlobalSetting
 
 def index(request):
     
@@ -8,11 +9,12 @@ def index(request):
     font_aktif = request.session.get('font_css', 'font-sans')
     
     is_group_member = check_is_group_member(request.user)
+    setting, created = GlobalSetting.objects.get_or_create(id=1)
             
     context = {
         'is_group_member': is_group_member,
-        'class_tema_aktif': tema_aktif,
-        'class_font_aktif': font_aktif,
+        'class_tema_aktif': setting.tema,
+        'class_font_aktif': setting.font,
     }
     
     return render(request, 'index.html', context)
@@ -20,21 +22,26 @@ def index(request):
 
 def ubah_tampilan_tema(request):
     if request.method == 'POST':
+        setting, created = GlobalSetting.objects.get_or_create(id=1)
 
         tema_sekarang = request.session.get('tema_css', '')
-        if tema_sekarang == '':
-            request.session['tema_css'] = 'dark-theme'
+        if setting.tema == '':
+                setting.tema = 'dark-theme'
         else:
-            request.session['tema_css'] = ''
+            setting.tema = ''
+        
+        setting.save()
     return redirect('index')
     
 def ubah_tampilan_font(request):
     if request.method == 'POST':
+        setting, created = GlobalSetting.objects.get_or_create(id=1)
 
         font_sekarang = request.session.get('font_css', '')
-        if font_sekarang == '':
-            request.session['font_css'] = 'serif-theme'
+        if setting.font == '':
+                setting.font = 'serif-theme'
         else:
-            request.session['font_css'] = ''
+            setting.font = ''
 
+        setting.save()
     return redirect('index')
